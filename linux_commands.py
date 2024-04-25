2e4f4ec7b66922ea02bce1bdb245a2d28e0f1029
Quantum211 <149906897+Quantum211@users.noreply.github.com>
2024-04-25 12:07:53 -0400

    Create linux_commands.py

---
+++ linux_commands.py
@@ -0,0 +1,5 @@
+commands = {
+  "ls" : "Lists all the visible files and directories located in the current working directory",
+  "cd" : "'cd' stands for change directory. Therefore, it moves to the specified directory. If left without parameters, moves to the user's home directory",
+  "grep" : "Completes a search based on the provided regular expression"
+}
