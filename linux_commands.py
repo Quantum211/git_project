905ad0e5423f50b18b67b52ffce37c102672dc42
Quantum211 <149906897+Quantum211@users.noreply.github.com>
2024-04-25 12:08:55 -0400

    Update linux_commands.py

--- linux_commands.py
+++ linux_commands.py
@@ -1,5 +1,6 @@
 commands = {
   "ls" : "Lists all the visible files and directories located in the current working directory",
   "cd" : "'cd' stands for change directory. Therefore, it moves to the specified directory. If left without parameters, moves to the user's home directory",
-  "grep" : "Completes a search based on the provided regular expression"
+  "grep" : "Completes a search based on the provided regular expression",
+  "tar" : "Command is used to create, display and execute archives that incorporate files"
 }
