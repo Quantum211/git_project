626b913fdb13091a23eaa2cca42269acfe6c8acb
nomad <daffodil8900@gmail.com>
2024-04-27 17:01:52 -0400

    Added new key - 'cp' to the dictionary - 'commands'

--- linux_commands.py
+++ linux_commands.py
@@ -2,7 +2,8 @@ commands = {
   "ls" : "Lists all the visible files and directories located in the current working directory",
   "cd" : "'cd' stands for change directory. Therefore, it moves to the specified directory. If left without parameters, moves to the user's home directory",
   "grep" : "Completes a search based on the provided regular expression",
-  "tar" : "Command is used to create, display and execute archives that incorporate files"
+  "tar" : "Command is used to create, display and execute archives that incorporate files",
+  "cp" : "Copies files and directories from one location to aanother"
 }
 
 print(commands)
