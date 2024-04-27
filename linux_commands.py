7f5c7756c84cc34d3abafa0e0defccd132385cbd
nomad <daffodil8900@gmail.com>
2024-04-27 17:04:11 -0400

    Added a new key, 'touch', to the dictionary 'commands'

--- linux_commands.py
+++ linux_commands.py
@@ -3,7 +3,8 @@ commands = {
   "cd" : "'cd' stands for change directory. Therefore, it moves to the specified directory. If left without parameters, moves to the user's home directory",
   "grep" : "Completes a search based on the provided regular expression",
   "tar" : "Command is used to create, display and execute archives that incorporate files",
-  "cp" : "Copies files and directories from one location to aanother"
+  "cp" : "Copies files and directories from one location to aanother",
+  "touch" : "Creates a new file. The filename is followed by the command as an argument"
 }
 
 print(commands)
