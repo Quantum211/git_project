eb8508cb3e4937805335b0a48d18dc769de6c144
nomad <daffodil8900@gmail.com>
2024-04-27 21:31:31 -0400

    Added a key called 'rmdir' to the 'commands' dictionary

--- linux_commands.py
+++ linux_commands.py
@@ -4,7 +4,8 @@ commands = {
   "grep" : "Completes a search based on the provided regular expression",
   "tar" : "Command is used to create, display and execute archives that incorporate files",
   "cp" : "Copies files and directories from one location to aanother",
-  "touch" : "Creates a new file. The filename is followed by the command as an argument"
+  "touch" : "Creates a new file. The filename is followed by the command as an argument",
+  "rmdir" : "Removes an empty directory"
 }
 
 print(commands)
